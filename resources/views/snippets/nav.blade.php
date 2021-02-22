762c8ff8c3aaae015030eb1d3b715b0de5a3003f
Arturo Martinez <arturo.martinez@disgo.pe>
2021-02-21 20:11:40 -0500

    nav finalizado

--- resources/views/snippets/nav.blade.php
+++ resources/views/snippets/nav.blade.php
@@ -42,13 +42,13 @@
                         <div id="barra" class="collapse navbar-collapse justify-content-end">
                             <!-- nav -->
                             <ul class="navbar-nav nav">
-                                <li class="nav-item"><a href="#" class="nav-link active">Inicio <span class="sr-only">(página actual)</span></a></li>
-                                <li class="nav-item"><a href="#" class="nav-link">Infraestructura</a></li>
-                                <li class="nav-item"><a href="#" class="nav-link">Horarios</a></li>
-                                <li class="nav-item"><a href="#" class="nav-link">Propuesta Pedagógica</a></li>
-                                <li class="nav-item"><a href="#" class="nav-link">Admisión</a></li>
-                                <li class="nav-item"><a href="#" class="nav-link">Noticias</a></li>
-                                <li class="nav-item"><a href="#" class="nav-link">Contacto</a></li>
+                                <li class="nav-item"><a href="{{route("home")}}" class="nav-link {{$menu=="home"?"active":""}}">Inicio{!!$menu=="home"?"<span class=\"sr-only\">(página actual)</span>":""!!}</a></li>
+                                <li class="nav-item"><a href="{{route("infraestructura")}}" class="nav-link {{$menu=="infraestructura"?"active":""}}">Infraestructura{!!$menu=="infraestructura"?"<span class=\"sr-only\">(página actual)</span>":""!!}</a></li>
+                                <li class="nav-item"><a href="{{route("horarios")}}" class="nav-link {{$menu=="horarios"?"active":""}}">Horarios{!!$menu=="horarios"?"<span class=\"sr-only\">(página actual)</span>":""!!}</a></li>
+                                <li class="nav-item"><a href="{{route("propuesta")}}" class="nav-link {{$menu=="propuesta"?"active":""}}">Propuesta Pedagógica{!!$menu=="propuesta"?"<span class=\"sr-only\">(página actual)</span>":""!!}</a></li>
+                                <li class="nav-item"><a href="{{route("admision")}}" class="nav-link {{$menu=="admision"?"active":""}}">Admisión{!!$menu=="admision"?"<span class=\"sr-only\">(página actual)</span>":""!!}</a></li>
+                                <li class="nav-item"><a href="{{route("noticias")}}" class="nav-link {{$menu=="noticias"?"active":""}}">Noticias{!!$menu=="noticias"?"<span class=\"sr-only\">(página actual)</span>":""!!}</a></li>
+                                <li class="nav-item"><a href="{{route("contacto")}}" class="nav-link {{$menu=="contacto"?"active":""}}">Contacto{!!$menu=="contacto"?"<span class=\"sr-only\">(página actual)</span>":""!!}</a></li>
                             </ul>
                         </div>
                         <!-- navbar-collapse -->
@@ -67,40 +67,3 @@
   </div>
 </header>
 <!--//================Header end==============//-->
-
-
-
-{{-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="{{route("home")}}">IEI Jesus Maestro</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="col-4"></div>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link {{$menu=="home"?"active":""}}" {{$menu=="home"?"aria-current=\"page\"":""}} href="{{route("home")}}">Inicio</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link {{$menu=="infraestructura"?"active":""}}" {{$menu=="infraestructura"?"aria-current=\"page\"":""}} href="{{route("infraestructura")}}">Infraestructura</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link {{$menu=="horarios"?"active":""}}" {{$menu=="horarios"?"aria-current=\"page\"":""}} href="{{route("horarios")}}">Horarios</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link {{$menu=="propuesta"?"active":""}}" {{$menu=="propuesta"?"aria-current=\"page\"":""}} href="{{route("propuesta")}}">Propuesta Pedagógica</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link {{$menu=="admision"?"active":""}}" {{$menu=="admision"?"aria-current=\"page\"":""}} href="{{route("admision")}}">Admisión</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link {{$menu=="noticias"?"active":""}}" {{$menu=="noticias"?"aria-current=\"page\"":""}} href="{{route("noticias")}}">Noticias</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link {{$menu=="contacto"?"active":""}}" {{$menu=="contacto"?"aria-current=\"page\"":""}} href="{{route("contacto")}}">Contacto</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav> --}}
